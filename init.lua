a7d81cdf13599af1ca3d3062f364bc55cbdfd4ab
Jörg Bakker <captaingroove@openmultimedia.org>
2023-05-30 14:54:50 +0200

    Cleanup, updated comments

--- init.lua
+++ init.lua
@@ -1,26 +1,25 @@
 --- Copyright (C) 2023  Jörg Bakker
 ---
---- FIXME vis doesn't restore the terminal properly after exiting when 'nnn' has been called
---- solutions:
---- 1. 'e' alias for vis always appends a call to the clear(1) command
---- 2. introduce save_screen_state() and restore_screen_state() functions in vis
----    for test code, see: /home/jorg/cc/testlab/curses/leave_curses.c
----    there already are functions ui_curses_save() and ui_curses_restore()
----    which are used in vis_pipe()
----    these new functions could be added to vis.h (also in lua vis module then ...?)
---- FIXME also need to hide the cursor when leaving 'nnn' with curs_set(0)
+--- FIXME vis doesn't restore the terminal properly after exiting when 'nnn' has been called.
+--- We should introduce save_screen_state() and restore_screen_state() functions in vis.
+--- For general curses test code, see: /home/jorg/cc/testlab/curses/leave_curses.c
+--- There already are functions ui_curses_save() and ui_curses_restore()
+--- which are used in vis_pipe() and do exactly this but setting the
+--- terminal to shell mode for the executed command
+--- TODO need to call curses functions: def_prog_mode(); endwin();
+--- TODO need to call curses function: reset_prog_mode() and
+---      hide the curses cursor after exiting nnn
+--- TODO make nnn options configurable
 module = {}
--- local curses = require("curses")
 
 vis:command_register("nnn", function(argv, force, win, selection, range)
-	--- TODO need to call curses functions: def_prog_mode(); endwin();
-	--- TODO need to hide the curses cursor after exiting nnn
-	--- TODO make nnn options configurable
-	--- TODO avoid recursive calls of nnn (vis -> nnn -> vis -> nnn ...)
-	-- curses.def_prog_mode()
-	-- curses.endwin()
     local pickfile_name = os.tmpname()
+
+	--- TODO better use the vis internal function to execute a shell command
+	--- as it handles terminal saving and restoring
+    -- vis:pipe(nil, nil, string.format("nnn -RuA -p %s", pickfile_name))
     os.execute(string.format("nnn -RuA -p %s", pickfile_name))
+
     local pickfile = io.open(pickfile_name)
     if not pickfile then
 	    vis:redraw()
@@ -35,8 +34,6 @@ vis:command_register("nnn", function(argv, force, win, selection, range)
     if success and output[1] ~= nil then
         vis:feedkeys(string.format(":e '%s'<Enter>", output[1]))
     end
-    --- TODO need to call curses function: reset_prog_mode()
-    -- curses.reset_prog_mode()
     vis:redraw()
     return true;
 end, "Select file to open with nnn")
